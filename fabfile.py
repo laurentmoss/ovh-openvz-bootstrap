ce980a8f7f9f270a54f20d41dd2f2c32eb2908da
Laurent Moss <laurent.moss@gmail.com>
2013-08-27 01:20:27 +0000

    Configure NAT for IP output from containers

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,7 @@
-from fabric.api import run
+from fabric.api import env, roles, run
 
-def host_type():
-    run('uname -a')
+env.roledefs['host'] = ['root@%s' % env.host_ip]
+
+@roles('host')
+def nat_ip_output():
+    run('iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -o vmbr0 -j SNAT --to %s' % env.host_ip)
